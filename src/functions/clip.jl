a78d22f131fcfa334374ca8d96ac18152a2b5d4b
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-19 09:40:52 +0000

    Snapshot of branch dev taken on 2019-05-19T05:40:50.344-04:00 from "https://github.com/UnofficialJuliaMirror/Merlin.jl-80f3d04f-b880-5e6d-8e06-6a7e799169ac"

--- src/functions/clip.jl
+++ src/functions/clip.jl
@@ -1,6 +1,6 @@
 export clipnorm!, clipvalue!
 
-function clipnorm!(x::Array{T}, threshold) where {T}
+function clipnorm!(x::Array{T}, threshold) where T
     (ndims(x) == 1 || ndims(x) == 2) || throw("Not supported yet.")
     z = mapreducedim(v -> v*v, +, x, 1)
     for j = 1:length(z)
@@ -10,7 +10,7 @@ function clipnorm!(x::Array{T}, threshold) where {T}
     x .*= z
 end
 
-function clipnorm!(x::CuArray{T}, threshold) where {T}
+function clipnorm!(x::CuArray{T}, threshold) where T
     (ndims(x) == 1 || ndims(x) == 2) || throw("Not supported yet.")
     z = mapreducedim(v -> v*v, +, x, 1)
     for j = 1:length(z)
@@ -20,9 +20,25 @@ function clipnorm!(x::CuArray{T}, threshold) where {T}
     x .*= z
 end
 
-function clipvalue!{T}(x::Array{T}, value)
+function clipvalue!(x::Array{T}, value::T) where T
     value = T(value)
     @inbounds for i = 1:length(x)
         x[i] = min(max(x[i],-value), value)
+    end
+end
+
+@generated function clipvalue!(x::CuArray{T}, value::T) where T
+    Ct = cstring(T)
+    k = Kernel("""
+    __global__ void clip($Ct *x, $Ct value, int n) {
+        int idx = blockIdx.x * blockDim.x + threadIdx.x;
+        if (idx >= n) return;
+        x[idx] = min(max(x[idx],-value), value);
+    }""")
+    quote
+        @assert value > T(0)
+        gdims, bdims = cudims(length(x))
+        $k(gdims, bdims, pointer(x), value, length(x)) # TODO: handle with int case
+        x
     end
 end
